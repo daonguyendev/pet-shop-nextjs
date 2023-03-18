88a4ecd5550d132f6d5aa05f22d8c77a8a5c96f3
daonguyendev <daonguyen.dev@gmail.com>
2023-03-19 03:30:20 +0700

    Init commit

---
+++ .next/static/webpack/pages/index.d12efd2734758c7a.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./src/data/gallary_data.json":
+/*!************************************!*\
+  !*** ./src/data/gallary_data.json ***!
+  \************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('[{"id":2,"imageBig":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-22.png","smallimage":"assets/images/bg/gallery/gallery-2.png"},{"id":3,"imageBig":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-33.png","smallimage":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-3.png"},{"id":4,"imageBig":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-44.png","smallimage":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-4.png"},{"id":5,"imageBig":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-55.png","smallimage":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-5.png"},{"id":6,"imageBig":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-66.png","smallimage":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-6.png"},{"id":7,"imageBig":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-77.png","smallimage":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-7.png"},{"id":8,"imageBig":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-88.png","smallimage":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-8.png"},{"id":9,"imageBig":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-99.png","smallimage":"https://scooby-nextjs.vercel.app/assets/images/bg/gallery/gallery-9.png"}]');
+
+/***/ })
+
+});
